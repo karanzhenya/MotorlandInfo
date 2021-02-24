ee9f528bbf00e072395068af99c5774ebdb405ba
karanzhenya <63317606+karanzhenya@users.noreply.github.com>
2021-02-24 07:37:47 +0300

    add info kia ceed

--- src/App.js
+++ src/App.js
@@ -26,46 +26,7 @@ export default function App(props) {
             <Grid container fixed>
                 <input onChange={changePassword} className={'password'} value={password} type={"password"}/>
             </Grid>
-
-            {password == "1111" ?<Grid item> <Button state={state}/> </Grid>: ""}
-            {/*{state.map(s => <Button id={s.id} data={data}/>)}*/}
-            {/* {visible ? state.map(s => <Button id={s.id} name={s.name} changeVisible={changeVisible}/>) :
-                <div>
-                    <button onClick={changeVisible}>Back</button>
-                    <br/>
-                    {state.map(b => <button onClick={() => {
-                        alert(b.model)
-                    }}>{"xxx"}</button>)}
-                </div>}
-                <div>
-                    <button onClick={changeVisible}>Back</button>
-                    <br/>
-                    <button onClick={() => {
-                        alert(state[17].info[0])
-                    }}>{state[17].model[1]}
-                    </button>
-                    <br/>
-                    <button onClick={() => {
-                        alert(state[17].info[1])
-                    }}>{state[17].model[2]}
-                    </button>
-                    <br/>
-                    <button onClick={() => {
-                        alert(state[17].info[2])
-                    }}>{state[17].model[3]}
-                    </button>
-                    <br/>
-                    <button onClick={() => {
-                        alert(state[17].info[3])
-                    }}>{state[17].model[5]}
-                    </button>
-                    <br/>
-                    <button onClick={() => {
-                        alert(state[17].info[4])
-                    }}>{state[17].model[6]}
-                    </button>
-                    <br/>
-                </div>*/}
+            {password == "1111" ? <Grid item> <Button state={state}/> </Grid> : ""}
         </div>
     )
 }
